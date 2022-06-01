2418fb4ab9aa2cd5cc309f79f149ebd12f6111ad
Catarina <catarina.lopes.04@hotmail.com>
2022-06-01 10:11:25 +0100

    Particle trigger + UI timer display

--- Assets/Scripts/GameUIController.cs
+++ Assets/Scripts/GameUIController.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 
@@ -5,6 +6,9 @@ public class GameUIController : MonoBehaviour
 {
     public TextMeshProUGUI killsText;
     public TextMeshProUGUI roundInfo;
+    public TextMeshProUGUI elapsedTimeText;
+
+    private float _timer;
 
     public void UpdateKills(int numberOfKills, int maxKills)
     {
@@ -14,5 +18,16 @@ public class GameUIController : MonoBehaviour
     public void UpdateRoundInfo(string info)
     {
         roundInfo.text = info;
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        
+        DateTime dateTime = new DateTime();
+        dateTime = dateTime.AddSeconds(_timer);
+
+        elapsedTimeText.text = string.Format("{0:00}:{1:00}:{2:00}", 
+            dateTime.Hour, dateTime.Minute, dateTime.Second);
     }
 }
---
+++ Assets/Scripts/ParticleTriggerTest.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParticleTriggerTest : MonoBehaviour
+{
+    public List<Collider> colliderList = new List<Collider>();
+    private ParticleSystem ps;
+
+
+    private void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        ps.trigger.AddCollider(colliderList[0]);
+    }
+
+    void OnParticleTrigger()
+    {
+        // particles
+        List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+        List<ParticleSystem.Particle> exit = new List<ParticleSystem.Particle>();
+
+        // get
+        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        int numExit = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+
+        // iterate
+        for (int i = 0; i < numEnter; i++)
+        {
+            ParticleSystem.Particle p = enter[i];
+            p.startColor = new Color32(255, 0, 0, 255);
+            enter[i] = p;
+        }
+
+        for (int i = 0; i < numExit; i++)
+        {
+            ParticleSystem.Particle p = exit[i];
+            p.startColor = new Color32(0, 255, 0, 255);
+            exit[i] = p;
+        }
+
+        // set
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+    }
+}
